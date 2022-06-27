8d15495f3cae0dbfc8af7281fe85eb9512392b30
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2022-06-27 04:38:24 +0300

    INIT

---
+++ js/script.js
@@ -0,0 +1,103 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance:
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/***
+ * `quotes` array
+ ***/
+const quotes = [
+  {
+    quote: `We cannot solve problems with the kind of thinking we employed when we came up with them.`,
+    source: `Albert Einstein`,
+    citation: `Personal Journal`,
+    year: `2021`,
+    tag: `Motivational`,
+  },
+  {
+    quote: `The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty`,
+    source: `Winston Churchill`,
+    citation: `The Interwebs`,
+    year: `1234 A.D.`,
+    tag: `Musical`,
+  },
+  {
+    quote: `Either you run the day or the day runs y ou`,
+    source: `Jim Rohn`,
+    citation: `Newspaper`,
+    year: `1545 A.D.`,
+    tag: `Educational`,
+  },
+  {
+    quote: `Look busy, stay busy, and you'll never be busy`,
+    source: `Matthew Pantel`,
+    citation: `Military`,
+    year: `2012`,
+    tag: `Deception`,
+  },
+  {
+    quote: `A man who commited a mistake and doesn't correct it is committing another mistake`,
+    source: `Confucius Kongzi`,
+    citation: `A&E`,
+    year: `2025 A.D.`,
+    tag: `Space Travel`,
+  },
+  {
+    quote: `Talent wins games, but teamwork and intelligence win championships`,
+    source: `Michael Jordan`,
+    citation: `ESPN`,
+    year: `1996`,
+    tag: `Teamwork`,
+  },
+  {
+    quote: `Be miserable. Or motivate yourself. Whatever has to be done, it's ALWAYS your choice`,
+    source: `Wayne Dyer`,
+    citation: `Books`,
+    year: `2030`,
+    tag: `Life`,
+  },
+  {
+    quote: `Pain is just weakness leaving the body`,
+    source: `USMC`,
+    citation: `History`,
+    year: `1776`,
+    tag: `Gardening`,
+  },
+  {
+    quote: `The standard you walk past, is the standard you accept`,
+    source: `David Hurley`,
+    citation: `Intranet`,
+    year: `1999`,
+    tag: `Fun`,
+  },
+  {
+    quote: `Heads over here...Bodies over there`,
+    source: `Venom`,
+    citation: `Film`,
+    year: `2018`,
+    tag: `Movie Quotes`,
+  },
+];
+
+/***
+ * `getRandomQuote` function
+ ***/
+function getRandomQuote() {}
+
+/***
+ * `printQuote` function
+ ***/
+function printQuote() {}
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+ ***/
+
+document
+  .getElementById("load-quote")
+  .addEventListener("click", printQuote, false);
